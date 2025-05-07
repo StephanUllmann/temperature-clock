de4aa9b50ab116279037e4f50073e48dceebb15d
Stephan Ullmann <stephan.ullmann@posteo.de>
2025-05-07 12:20:51 +0200

    temperature color

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,6 @@
 export const getTemperatureColor = (temp) => {
   if (temp > 30) return 'hsl(0 80 50)';
+  // let hue = (15 - temp) * 12;
   let hue = (30 - temp) * 6;
   return `hsl(${Math.round(hue)} 80 50% / 0.75)`;
 };
